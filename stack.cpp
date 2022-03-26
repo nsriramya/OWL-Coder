70409c573f89f297f4182f5ab3d8aef642a4f50a
NALLAMSETTY SRI RAMYA <71599848+nsriramya@users.noreply.github.com>
2022-03-26 18:58:52 +0530

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	stack<int>s;
+	s.push(1);
+	s.emplace(8);
+	s.push(7);
+	s.emplace(2);
+	s.push(6);
+	s.emplace(3);
+	s.push(0);
+	s.emplace(8);
+	while(!s.empty())
+	{
+	cout<<s.top()<<" ";
+	s.pop();
+}
+	s.size();
+	stack<int>s1,s2;
+	s1.swap(s2);
+	 
+	 	 }
